9813b1042919d6a8ba35517288f578b08a21c0cd
HirudineeADRT <hirudinee@adroitlogic.com>
2020-04-08 18:17:41 +0530

    first

--- thistestfile/thirdfunction.py
+++ thistestfile/thirdfunction.py
@@ -1,11 +1,12 @@
 import boto3
+import os
 sqs = boto3.client("sqs")
 
 def handler(event, context):
 
     try:
         data = sqs.receive_message(
-            QueueUrl="https://sqs.{}.amazonaws.com/{}/hiru-test".format(environ["AWS_REGION"], environ["SIGMA_AWS_ACC_ID"]),
+            QueueUrl="https://sqs.{}.amazonaws.com/{}/hiru-test".format(os.environ["AWS_REGION"], os.environ["SIGMA_AWS_ACC_ID"]),
             MaxNumberOfMessages=1,
             VisibilityTimeout=30,
             WaitTimeSeconds=0,
